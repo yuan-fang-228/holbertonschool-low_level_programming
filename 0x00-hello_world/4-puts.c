f43bb3bc81c781ac691fed49d06fb0e52d4f0b50
yuan-fang-228 <fy0228@gmail.com>
2022-02-03 21:56:53 +1100

    Fixed the source file for task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@
 /* use puts to print out a string*/
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
