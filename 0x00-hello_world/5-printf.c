a1f8016c780bba9c173a52a68a1ce1ecf6806bf4
yuan-fang-228 <fy0228@gmail.com>
2022-02-03 22:13:12 +1100

    Added source file for task 5 and removed testing files

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Use printf to print a string followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
