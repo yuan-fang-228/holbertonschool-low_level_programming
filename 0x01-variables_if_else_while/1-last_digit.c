abf5855efe75df17baf9362e4d977327ca23f0c7
yuan-fang-228 <fy0228@gmail.com>
2022-02-04 13:40:57 +1100

    fixed minus number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, lastDig);
 	}
-	else
+	else if (lastDig >= 6)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDig);
 	}
