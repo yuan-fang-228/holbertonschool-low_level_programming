ccf5aecb1ce76444dcaf2c3411ed2fd8ab555856
yuan-fang-228 <fy0228@gmail.com>
2022-02-04 21:20:35 +1100

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - use putchar to prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = '0';
+	int h = 'a';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	while (h <= 'f')
+	{
+		putchar(h);
+		h++;
+	}
+
+	putchar('\n');
+}
+
