96e4ad7a0cf52b739b7e5942e3b0e3e7121086fb
yuan-fang-228 <fy0228@gmail.com>
2022-02-04 23:38:21 +1100

    task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - Print "Programming is like building a multilingual puzzle
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
