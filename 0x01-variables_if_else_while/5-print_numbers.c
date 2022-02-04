844a809399f33d377848d0f254b2512d659bf955
yuan-fang-228 <fy0228@gmail.com>
2022-02-04 14:00:56 +1100

    0x01. C - Variables, if, else, while task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print all single digit;
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+		printf("\n");
+	return (0);
+}
+
