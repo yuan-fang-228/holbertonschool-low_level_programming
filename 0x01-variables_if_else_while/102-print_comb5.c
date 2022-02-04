cdc93bffcea08196d5abc3a0aa0951ceced193de
yuan-fang-228 <fy0228@gmail.com>
2022-02-04 22:58:40 +1100

    task12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints all possible different combinations of four digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <='9'; c++)
+			{
+				for (d = b+1; d <= '9'; d++)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+					if(!(a == '9' && b == '8' && c == '9' && d == '9'))
+					{
+						putchar(',');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
