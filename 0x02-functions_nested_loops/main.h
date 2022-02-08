953964e89b73877b1cb4a1170fc9e3c4401dd3f5
yuan-fang-228 <fy0228@gmail.com>
2022-02-08 12:26:11 +1100

    task 0 and task 1

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* print the characters */
+int _putchar(char c);
+
+/* print the alphabet in lowercase */
+void print_alphabet();
+
+/* print 10 times the alphabet in lowercase*/
+void print_alphabet_x10();
+
+#endif
