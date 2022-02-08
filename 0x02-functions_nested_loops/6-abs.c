365c868ba7a0d64717c8e9bacae95d711c2484b4
yuan-fang-228 <fy0228@gmail.com>
2022-02-08 14:01:46 +1100

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - computes the absolute value of integer
+ * @i: The number to computer
+ *
+ * Return: return the absolute value of this integer
+ */
+int _abs(int i)
+{
+	return (abs(i));
+}
