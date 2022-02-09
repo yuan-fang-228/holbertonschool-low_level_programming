51ec6ad831b0ec9047e15e368c5b7142ee3982d3
yuan-fang-228 <fy0228@gmail.com>
2022-02-09 23:10:46 +1100

    task 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -9,4 +9,10 @@ void positive_or_negative(int i);
 /* prints the largest of three integers */
 int largest_number(int a, int b, int c);
 
+/* calculate the day of the year,without considering leap year */
+int convert_day(int month, int day);
+
+/* calculate how many days remaining */
+void print_remaining_days(int month, int day, int year);
+
 #endif /* MAIN_H */
