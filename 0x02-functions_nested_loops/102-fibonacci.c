0a4ff04f09d40af0bb3cda71e73580ac88a544d1
yuan-fang-228 <fy0228@gmail.com>
2022-02-09 23:55:33 +1100

    Fixed the compilation for task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	int f1 = 0, f2 = 1, i;
+	int f1 = 0, f2 = 1, i, next;
 
     printf("%d ", f1);
     for (i = 1; i < 50; i++) {
         printf("%d ", f2);
-        int next = (f1 + f2);
+        next = (f1 + f2);
         f1 = f2;
         f2 = next;
     }
