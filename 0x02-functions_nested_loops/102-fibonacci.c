ca14112db9dd1eae64a29f9e679d876914692bbf
yuan-fang-228 <fy0228@gmail.com>
2022-02-09 23:52:48 +1100

    Added the source file for 0x02 task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the first 50 Fibonacci numbers
+ *
+ * Return: return the result
+ */
+int main(void)
+{
+	int f1 = 0, f2 = 1, i;
+
+    printf("%d ", f1);
+    for (i = 1; i < 50; i++) {
+        printf("%d ", f2);
+        int next = f1 + f2;
+        f1 = f2;
+        f2 = next;
+    }
+	return (0);
+}
