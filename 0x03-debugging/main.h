4254e484d9e13d71114a154bcbafd2eeb28be13a
yuan-fang-228 <fy0228@gmail.com>
2022-02-09 22:08:05 +1100

    task 2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,10 @@
 
 #include <stdio.h>
 
+/* check the number positive or negative */
 void positive_or_negative(int i);
+
+/* prints the largest of three integers */
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
