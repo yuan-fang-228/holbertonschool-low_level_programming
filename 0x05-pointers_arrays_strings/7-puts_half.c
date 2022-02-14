5b94c02bb53049c088b4189287b225c7cd923410
yuan-fang-228 <fy0228@gmail.com>
2022-02-14 15:53:44 +1100

    Simplified the task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,19 +13,9 @@ void puts_half(char *str)
 	int i = 0;
 	int length = strlen(str);
 
-	if (length % 2 == 0)
+	for (i = (length + 1) / 2; i < length; i++)
 	{
-		for (i = length / 2; i < length; i++)
-		{
-			_putchar(str[i]);
-		}
-	}
-	else
-	{
-		for (i = (length - 1) / 2 ; i <= length; i++)
-		{
-			_putchar(str[i + 1]);
-		}
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
