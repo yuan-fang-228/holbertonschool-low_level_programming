0c60078fa17a8b74190c878e16a9e6ed068c127d
yuan-fang-228 <fy0228@gmail.com>
2022-02-14 12:51:01 +1100

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+  * _strlen - returns the length of a string.
+  * @s: string to check
+  *
+  * Return: return the length.
+  */
+int _strlen(char *s)
+{
+	int lengthOfString;
+
+	lengthOfString = strlen(s);
+
+	return (lengthOfString);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* takes a pointer to an int as parameter and update value*/
+/* takes a pointer to an int as parameter and update value */
 void reset_to_98(int *n);
+
+/* swaps the value of two integers */
+void swap_int(int *a, int *b);
+
+/* return the length of a string */
+int _strlen(char *s);
 
 #endif
