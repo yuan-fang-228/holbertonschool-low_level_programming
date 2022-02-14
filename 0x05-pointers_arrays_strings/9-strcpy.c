7fa677ffff914d75ef73e38da55a10311d760fd6
yuan-fang-228 <fy0228@gmail.com>
2022-02-14 23:31:27 +1100

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,12 +8,14 @@
   */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
-	int length = strlen(src);
+	int i = 0;
 
-	for (i = 0; i < length; i++)
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
+		i++;
 	}
+	dest[i] = '\0';
+
 	return (dest);
 }
