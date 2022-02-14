288f16e27b0ccef906e0cc319904a8676ce7b6f5
yuan-fang-228 <fy0228@gmail.com>
2022-02-14 15:22:28 +1100

    task 7 fixed edge cases

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,10 +19,13 @@ void puts_half(char *str)
 		{
 			_putchar(str[i]);
 		}
-			_putchar('\n');
 	}
 	else
 	{
-		_putchar(str[(length - 1) / 2]);
+		for (i = (length - 1) / 2; i < length; i++)
+		{
+			_putchar(str[i]);
+		}
 	}
+	_putchar('\n');
 }
