9ef2c82fb70dcb026ba80e4e10fd9b87990f3e93
yuan-fang-228 <fy0228@gmail.com>
2022-02-14 13:21:22 +1100

    fixed task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,12 +2,17 @@
 
 /**
   * swap_int - swap the values of two integers
-  * @a: integer a
-  * @b: integer b
+  * @a: integer pointer a
+  * @b: integer poitner b
   *
   * Return: return null.
   */
 void swap_int(int *a, int *b)
 {
+	int aa;
+
+	aa = *a;
 	*a = *b;
+	*b = aa;
+	
 }
