979edaf5096778f8955403fae4aa0197433ee34a
yuan-fang-228 <fy0228@gmail.com>
2022-02-14 14:45:55 +1100

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+  * puts2 - print every other character of a string
+  * @str: string to print
+  *
+  * Return: return null
+  */
+void puts2(char *str)
+{
+	int i = 0;
+	int length = strlen(str);
+
+	while (i >= 0 && i <= length)
+	{
+		_putchar(str[i]);
+		i = i + 2;
+	}
+	_putchar('\n');
+}
