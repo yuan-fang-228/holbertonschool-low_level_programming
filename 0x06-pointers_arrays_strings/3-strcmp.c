de1a16034f7d2b1282845f38a81c97a00a9e5249
yuan-fang-228 <fy0228@gmail.com>
2022-02-17 01:22:56 +1100

    fix betty

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,22 +3,19 @@
   * _strcmp - compare two strings
   * @s1: string1 to compare
   * @s2: string2 to compare
-  * 
   * Return: return difference ASCII
   */
 
 int _strcmp(char *s1, char *s2)
 {
-
-    while((*s1!='\0') || (*s2!='\0'))
-    {
-
-        if (*s1 != *s2)
-        {
-            return (*s1 - *s2);
-        }
-        s1++;
-        s2++;
-    }
-    return 0;
+	while ((*s1 != '\0') || (*s2 != '\0'))
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+	}
+	return (0);
 }
