9631d15bd4bfe17f5d15f1105224494168fe85b1
yuan-fang-228 <fy0228@gmail.com>
2022-02-17 00:00:15 +1100

    task 0 + 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+  * _strlen - returns th length of a string
+  * @s: string to check
+  * Return: return the length
+  */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+  * _strcat - appends the src string to the dest string
+  * @dest: destination string
+  * @src: source string
+  * Return: return the dest string
+  */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int length = _strlen(dest);
+
+	while (src[i] != '\0')
+	{
+		dest[length + i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
+
+/**
+  * _strncat - concatenates 2 strings use n bytes from src
+  * @dest: destination string
+  * @src: source string
+  * @n: bytes from src
+  * Return: return dest string.
+  */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int lengthDest = _strlen(dest);
+
+	if (_strlen(src) <= n)
+	{
+		_strcat(dest, src);
+	}
+	while (i < n)
+	{
+		dest[lengthDest + i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*  concatenates two strings */
+char *_strcat(char *dest, char *src);
+
+/* concatenates two strings only use at most n bytes from src */
+char *_strncat(char *dest, char *src, int n);
+
+#endif
