28bcd843ca8db5ced8221da0da20c2683e05160a
yuan-fang-228 <fy0228@gmail.com>
2022-02-17 01:18:01 +1100

    task 3

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,4 +7,10 @@ char *_strcat(char *dest, char *src);
 /* concatenates two strings only use at most n bytes from src */
 char *_strncat(char *dest, char *src, int n);
 
+/* copy a string and use '0\' to fill out the test */
+char *_strncpy(char *dest, char *src, int n);
+
+/* compare two strings */
+int _strcmp(char *s1, char *s2);
+
 #endif
