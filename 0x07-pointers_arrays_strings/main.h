43ec8059b4b18751cf691fe33f1a11e60c0ae5da
yuan-fang-228 <fy0228@gmail.com>
2022-02-21 12:19:04 +1100

    Added task 1 and 2 for 0x07

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* fills memory with a constant byte */
+char *_memset(char *s, char b, unsigned int n);
+
+/* copy memory area */
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+#endif
