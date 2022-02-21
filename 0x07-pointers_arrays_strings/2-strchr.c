eb9e12ff883a61c995ec366372dadda51946f591
yuan-fang-228 <fy0228@gmail.com>
2022-02-21 15:41:59 +1100

    fixed while loop

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,15 +11,12 @@ char *_strchr(char *s, char c)
 {
 	int count = 0;
 
-	while (s[count] != '\0')
+	while (s[count] != '\0' && s[count] != c)
 	{
+		count++;
 		if (s[count] == c)
 		{
 			return (&s[count]);
-		}
-		else
-		{
-			count++;
 		}
 	}
 	return (NULL);
