12953b6666d1bb8320cc55ad97f6d8693bfc7da8
yuan-fang-228 <fy0228@gmail.com>
2022-02-21 14:31:23 +1100

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+  * _strlen - returns th length of a string
+  * @s: string to check
+  * Return: return the length
+  */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+  * _strchr - locate a character in a string
+  * @s: pointer to a string
+  * @c: character to be located in the string
+  * Return: return the address of the character if yes, otherwise return null.
+  */
+char *_strchr(char *s, char c)
+{
+	int count;
+	int length = _strlen(s);
+
+	for (count = 0; count < length; count++)
+	{
+		if (s[count] == c)
+		{
+			return (&s[count]);
+		}
+	}
+	return (0);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,4 +7,7 @@ char *_memset(char *s, char b, unsigned int n);
 /* copy memory area */
 char *_memcpy(char *dest, char *src, unsigned int n);
 
+/* locate a character in a string */
+char *_strchr(char *s, char c);
+
 #endif
