0e3701367ced4bc9676941f75836b0dce533c326
yuan-fang-228 <fy0228@gmail.com>
2022-02-23 11:48:36 +1100

    task 2

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,4 +10,7 @@ void _puts_recursion(char *s);
 /* print a string in reverse */
 void _print_rev_recursion(char *s);
 
+/* return the length of a string */
+int _strlen_recursion(char *s);
+
 #endif
