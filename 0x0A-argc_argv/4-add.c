64d2737e89f6c56308bd890db83dfd3a08f66ed6
yuan-fang-228 <fy0228@gmail.com>
2022-02-25 17:10:16 +1100

    replace isdigit function

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
   * isadigit - check if it is digit
@@ -12,7 +11,7 @@ int isadigit(char *c)
 {
 	while (*c != '\0')
 	{
-		if (isdigit(*c) == 1)
+		if (*c >= '0' && *c <= '9')
 			c++;
 		else
 			return (0);
