7614388aad4a14a88150344edf503bd0ff4bf934
yuan-fang-228 <fy0228@gmail.com>
2022-02-25 15:58:58 +1100

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+  * isadigit - check if it is digit
+  * @c: the string to check
+  * Return: return 1 if digit, otherwise return 0
+  */
+int isadigit(char *c)
+{
+	while (*c != '\0')
+	{
+		if (isdigit(*c) == 1)
+			c++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+/**
+  * main - adds positive numbers
+  * @argc: the number of the command line arguments
+  * @argv: the array of the command line arguments
+  * Return: return 1 if not digits, otherwise return 0
+  */
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (isadigit(argv[i]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum = sum + atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+}
