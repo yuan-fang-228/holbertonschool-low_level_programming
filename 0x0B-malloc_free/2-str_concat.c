f3bf03a09456b77b6eb4be65d9416310ad413046
yuan-fang-228 <fy0228@gmail.com>
2022-03-01 14:01:21 +1100

    task 2 added else if

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -42,9 +42,13 @@ char *str_concat(char *s1, char *s2)
 	{
 		s1 = "";
 	}
-	else if (s2 == NULL)
+	if (s2 == NULL)
 	{
 		s2 = "";
+	}
+	else if (s1 == NULL && s2 == NULL)
+	{
+		return (NULL);
 	}
 	length1 = _strlen(s1);
 	length2 = _strlen(s2);
