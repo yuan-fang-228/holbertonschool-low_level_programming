517ff62e528b097ab3c0bb9808281c443f5494dc
yuan-fang-228 <fy0228@gmail.com>
2022-03-01 22:32:59 +1100

    updated main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -13,4 +13,10 @@ char *_strdup(char *str);
 /* concatenates two strings */
 char *str_concat(char *s1, char *s2);
 
+/* returns a pointer to a 2 dimensional array of integers */
+int **alloc_grid(int width, int height);
+
+/* frees a 2 dimensional grid */
+void free_grid(int **grid, int height);
+
 #endif
