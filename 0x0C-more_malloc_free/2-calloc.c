02b9d80a01923f2f2d02b858ee630e11a2b4e596
yuan-fang-228 <fy0228@gmail.com>
2022-03-03 13:40:43 +1100

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * _calloc - allocate memory of an array
+  * @nmemb: elements amount of the array
+  * @size: bytes
+  * Return: a pointer to allocated memory
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *pointer;
+	char *tmpPointer;
+	unsigned int i = 0;
+
+	if (nmemb == 0)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+	pointer = malloc(nmemb * size);
+	if (pointer == NULL)
+		return (NULL);
+	tmpPointer = pointer;
+	while (i < nmemb * size)
+	{
+		tmpPointer[i] = 0;
+		i++;
+	}
+	pointer = tmpPointer;
+
+	return (pointer);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,4 +7,7 @@ void *malloc_checked(unsigned int b);
 /* concatenates two strings */
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 
+/* allocates memory for an array */
+void *_calloc(unsigned int nmemb, unsigned int size);
+
 #endif
