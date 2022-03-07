a71fb0ba905336613871c14fdd5cf4faed79e426
yuan-fang-228 <fy0228@gmail.com>
2022-03-07 03:02:15 -0800

    added null condition

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - initialize a variable of type struct dog
@@ -9,6 +10,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		return;
+	}
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
