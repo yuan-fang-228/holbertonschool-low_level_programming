b279d8644299dff5a1d2f9d5c74280fe1fb0144a
yuan-fang-228 <fy0228@gmail.com>
2022-03-07 03:36:19 -0800

    task 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,7 @@ struct dog
 /* initialize a varible of type struct dog */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
+/* print a struct dog */
+void print_dog(struct dog *d);
+
 #endif
