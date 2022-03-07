a3b3a57e29eddc6fa38a7a6071bd456f6dba79fd
yuan-fang-228 <fy0228@gmail.com>
2022-03-07 23:57:50 +1100

    update task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,30 +22,6 @@ char *_strcpy(char *dest, char *src)
 }
 
 /**
-  * _strlen - returns the length of a string.
-  * @s: string to check
-  *
-  * Return: return the length.
-  */
-int _strlen(char *s)
-{
-	int i;
-	int length;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		length = i;
-	}
-
-	if (length > 0)
-	{
-		length++;
-	}
-
-	return (length);
-}
-
-/**
   * new_dog - a function that creates a new dog
   * @name: dog name
   * @age: dog age
@@ -63,7 +39,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	if (name != NULL)
 	{
-		copyName = malloc(_strlen(name) + 1);
+		copyName = malloc(sizeof(*copyName));
 		if (copyName == NULL)
 		{
 			free(newDog);
@@ -76,7 +52,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	(*newDog).age = age;
 	if (owner != NULL)
 	{
-		copyOwner = malloc(_strlen(owner) + 1);
+		copyOwner = malloc(sizeof(*copyOwner));
 		if (copyOwner == NULL)
 		{
 			free(copyName);
