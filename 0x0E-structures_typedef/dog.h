0ae58f83f2929f6e8d70cb9e86732afcd2a40094
yuan-fang-228 <fy0228@gmail.com>
2022-03-07 23:56:17 +1100

    Added header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,4 +24,10 @@ void print_dog(struct dog *d);
 /* Define a new type dog_t as a new name for the type struct dog */
 typedef struct dog dog_t;
 
+/* a function that free dogs */
+void free_dog(dog_t *d);
+
+/* a function that creates a new dog */
+dog_t *new_dog(char *name, float age, char *owner);
+
 #endif
