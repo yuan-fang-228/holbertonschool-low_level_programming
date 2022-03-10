833fd29a73a4f1f6aca33ce44f3a58b097ba3308
yuan-fang-228 <fy0228@gmail.com>
2022-03-10 22:46:48 +1100

    fixed task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,44 +10,39 @@
   */
 void print_all(const char * const format, ...)
 {
-	char *separator = "", *formats = "cifs", *s;
-	int i, j = 0;
+	char *separator = "", *s;
+	int i = 0;
 	va_list vl;
 
 	va_start(vl, format);
-	while (format != NULL && format[j] != '\0')
+	while (format != NULL && format[i] != '\0')
 	{
-		i = 0;
-		while (formats[i] != '\0')
+		printf("%s", separator);
+		switch (format[i])
 		{
-			if (formats[i] == format[j])
-			{
-				printf("%s", separator);
-				switch (format[j])
-				{
-					case 'c':
-						printf("%c", va_arg(vl, int));
-						break;
-					case 'i':
-						printf("%d", va_arg(vl, int));
-						break;
-					case 'f':
-						printf("%f", va_arg(vl, double));
-						break;
-					case 's':
-						s = va_arg(vl, char *);
-						if (s == NULL)
-							s = "(nil)";
-						printf("%s", s);
-						break;
-					default:
-						break;
-				}
+			case 'c':
+				printf("%c", va_arg(vl, int));
 				separator = ", ";
-			}
-			i++;
+				break;
+			case 'i':
+				printf("%d", va_arg(vl, int));
+				separator = ", ";
+				break;
+			case 'f':
+				printf("%f", va_arg(vl, double));
+				separator = ", ";
+				break;
+			case 's':
+				s = va_arg(vl, char *);
+				if (s == NULL)
+					s = "(nil)";
+				printf("%s", s);
+				separator = ", ";
+				break;
+			default:
+				break;
 		}
-		j++;
+		i++;
 	}
 	va_end(vl);
 	printf("\n");
