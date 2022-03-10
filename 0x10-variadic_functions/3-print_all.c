fdd5424195ce1957482007b3b5549e934567f296
yuan-fang-228 <fy0228@gmail.com>
2022-03-10 22:36:42 +1100

    fixed the betty

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,53 +5,49 @@
 
 /**
   * print_all - print anything
-  * @format: type of arguments 
+  * @format: type of arguments
   * Return: void
   */
 void print_all(const char * const format, ...)
 {
-    char *separator = "";
-    int i, j = 0;
-    va_list vl;
+	va_list vl;
+	char *separator = "", *formats = "cifs", *s = va_arg(vl, char *);
+	int i, j = 0;
 
-    char *formats = "cifs";
-
-    va_start(vl, format);
-    while (format != NULL && format[j] != '\0')
-    {
-        i = 0;
-        while (formats[i] != '\0')
-        {
-            if (formats[i] == format[j])
-            {
-                char *s;
-                printf("%s", separator);
-                switch (format[j])
-                {
-                    case 'c':
-                        printf("%c", va_arg(vl, int));
-                        break;
-                    case 'i':
-                        printf("%d", va_arg(vl, int));
-                        break;
-                    case 'f':
-                        printf("%f", va_arg(vl, double));
-                        break;
-                    case 's':
-                        s = va_arg(vl, char *);
-                        if (s == NULL)
+	va_start(vl, format);
+	while (format != NULL && format[j] != '\0')
+	{
+		i = 0;
+		while (formats[i] != '\0')
+		{
+			if (formats[i] == format[j])
+			{
+				printf("%s", separator);
+				switch (format[j])
+				{
+					case 'c':
+						printf("%c", va_arg(vl, int));
+						break;
+					case 'i':
+						printf("%d", va_arg(vl, int));
+						break;
+					case 'f':
+						printf("%f", va_arg(vl, double));
+						break;
+					case 's':
+						if (s == NULL)
 							s = "(nil)";
-                        printf("%s", s);
-                        break;
-                    default:
-                        break;
-                }
-                separator = ", ";
-            }
-            i++;
-        }
-        j++;
-    }
-    va_end(vl);
-    printf("\n");
+						printf("%s", s);
+						break;
+					default:
+						break;
+				}
+				separator = ", ";
+			}
+			i++;
+		}
+		j++;
+	}
+	va_end(vl);
+	printf("\n");
 }
