387cdfa8352d9a2186d50562f835996d562eb806
yuan-fang-228 <fy0228@gmail.com>
2022-03-10 12:36:59 +1100

    changed condition of print separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	}
 	for (i = 0; i < n; i++)
 	{
-		printf("%d%s ", va_arg(vl, int), separator);
+		if (i == n - 1)
+		{
+			printf("%d", va_arg(vl, int));
+		}
+		else
+		{
+			printf("%d%s", va_arg(vl, int), separator);
+		}
 	}
 	printf("\n");
 
