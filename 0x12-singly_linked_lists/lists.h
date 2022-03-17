c59d0818f08ac52b45f3a18e8770dc9a1d2b07e0
yuan-fang-228 <fy0228@gmail.com>
2022-03-17 12:18:41 +1100

    added header file

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+/* print the characters */
+int _putchar(char c);
+
+/* print all the elements of a list_t list */
+size_t print_list(const list_t *h);
+
+#endif
