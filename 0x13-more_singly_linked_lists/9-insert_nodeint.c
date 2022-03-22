0baf547710c229f3a4d3171c1112dd0c9786aa16
yuan-fang-228 <fy0228@gmail.com>
2022-03-22 14:14:10 +1100

    fixed betty

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head == NULL || *head == NULL)
 		return (NULL);
-	if(*head == NULL & idx != 0)
+	if (*head == NULL & idx != 0)
 		return (NULL);
 	newNode = malloc(sizeof(*newNode));
 	if (newNode == NULL)
