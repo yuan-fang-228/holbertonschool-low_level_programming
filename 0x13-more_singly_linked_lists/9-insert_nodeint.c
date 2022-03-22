fb0cef2ad42d7ea530b492250163aa28ef5399cd
yuan-fang-228 <fy0228@gmail.com>
2022-03-22 14:05:18 +1100

    add edge cases

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,6 +30,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (i <= (idx - 1))
 	{
 		tmp = tmp->next;
+		if (tmp == NULL)
+		{
+			return (NULL);
+		}
 		i++;
 	}
 	newNode->next = tmp->next;
