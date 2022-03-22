87406ada3f5674ab4461ec4d25054be1701a6ec4
yuan-fang-228 <fy0228@gmail.com>
2022-03-22 14:02:14 +1100

    changed while condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (newNode);
 	}
 	tmp = *head;
-	while (i <= idx)
+	while (i <= (idx - 1))
 	{
 		tmp = tmp->next;
 		i++;
