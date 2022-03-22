81537289f59b47636e7f18e946e1acd6f02f844a
yuan-fang-228 <fy0228@gmail.com>
2022-03-22 14:38:31 +1100

    add free

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,8 +33,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		tmp = tmp->next;
 		if (tmp == NULL)
+		{
 			free(newNode);
 			return (NULL);
+		}
 		i++;
 	}
 	newNode->next = tmp->next;
