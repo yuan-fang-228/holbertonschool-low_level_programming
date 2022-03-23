39d24fea40ac9cfde53752019178f52ef133acb5
yuan-fang-228 <fy0228@gmail.com>
2022-03-23 22:56:43 +1100

    task 12

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  * print_listint_safe - print a linked list safe version
+  * @head: pointer to head node
+  * Return: the number of nodes in the list
+  */
+size_t print_listint_safe(const listint_t *head)
+{
+	int count = 0;
+	long int add1, add2;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		add1 = (long int)head;
+		add2 = (long int)head->next;
+		printf("[%p] %d\n", (void *)head, head->n);
+		count++;
+		if (add1 - add2 > 0)
+		{
+			head = head->next;
+		}
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
+	}
+	return (count);
+}
