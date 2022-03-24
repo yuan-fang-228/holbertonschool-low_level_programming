7945f09bdc3244d4fa763a90a958622861181247
yuan-fang-228 <fy0228@gmail.com>
2022-03-24 22:05:08 +1100

    updated header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,4 +10,16 @@ unsigned int binary_to_uint(const char *b);
 /* print binary representation of a number */
 void print_binary(unsigned long int n);
 
+/* return the value of a bit at a given index */
+int get_bit(unsigned long int n, unsigned int index);
+
+/* set the value of a bit to 1 at a given index */
+int set_bit(unsigned long int *n, unsigned int index);
+
+/* set the value of a bit to 0 at a given index */
+int clear_bit(unsigned long int *n, unsigned int index);
+
+/* returns the number of bits you would need to flip to get from one number to another */
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
 #endif
