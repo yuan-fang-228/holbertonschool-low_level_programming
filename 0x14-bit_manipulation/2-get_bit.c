7b1576e541cb784b860a03f6869b10b4dd0aa9b0
yuan-fang-228 <fy0228@gmail.com>
2022-03-24 22:04:30 +1100

    task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * get_bit - return the value of a bit at a given index
+  * @n: number to check
+  * @index: index
+  * Return: the bit at the given index
+  */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= 32)
+		return (-1);
+	return ((n >> index) & 1);
+}
