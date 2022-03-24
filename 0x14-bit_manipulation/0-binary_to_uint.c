724153094caa231a5dbef01932dfe5800686bcf5
yuan-fang-228 <fy0228@gmail.com>
2022-03-24 13:39:10 +1100

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _power - calculate the power of a number
+ * @p: power number
+ * @b: base number
+ * Return: return result
+ */
+int _power(int b, int p)
+{
+	int i;
+	int result = 1;
+
+	for (i = 1; i <= p; i++)
+	{
+		result = result * b;
+	}
+
+	return (result);
+}
+
+/**
+  * _strlen - returns th length of a string
+  * @s: string to check
+  * Return: return the length
+  */
+int _strlen(const char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+  * binary_to_uint - convert a binary number to unsigned int
+  * @b: binary number
+  * Return: unsigned int
+  */
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0;
+	int power;
+	unsigned int decimal = 0;
+
+	if (b == NULL)
+		return (0);
+	power = _strlen(b) - 1;
+	while (b[i] != '\0')
+	{
+		if ((b[i] != '0') && b[i] != '1')
+		{
+			return (0);
+		}
+		if (b[i] == '1')
+		{
+			decimal = decimal + _power(2, power);
+		}
+		i++;
+		power--;
+	}
+
+	return (decimal);
+}
