f64af404ca67bd2b5b647fc808dd32871097e02d
yuan-fang-228 <fy0228@gmail.com>
2022-03-24 23:50:45 +1100

    task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,15 +9,15 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int difference;
-	int i;
 	int count = 0;
 
 	difference = n ^ m;
-	for (i = 0; i <= 32; i++)
+	while (difference > 0)
 	{
 		if (difference & 1)
 			count++;
 		difference = difference >> 1;
 	}
+
 	return (count);
 }
