cb0a312db90bf4d400b31de5bba41723adbcd488
yuan-fang-228 <fy0228@gmail.com>
2022-03-24 21:33:58 +1100

    task 1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* print the characters */
+int _putchar(char c);
+
 /* convert a binary number to unsigned int */
 unsigned int binary_to_uint(const char *b);
+
+/* print binary representation of a number */
+void print_binary(unsigned long int n);
 
 #endif
