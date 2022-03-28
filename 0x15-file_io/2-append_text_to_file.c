2de91d827f234d9a919c5aa3721ddc7bf95cc9e5
yuan-fang-228 <fy0228@gmail.com>
2022-03-28 16:36:05 +1100

    added context=null

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -40,8 +40,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
+	if (text_content == NULL)
+		return (1);
 	length = _strlen(text_content);
-	writebytes = write(fd, text_content, length);
+	writebytes = write(fd, text_content, length + 1);
 	if (writebytes == -1)
 	{
 		close(fd);
