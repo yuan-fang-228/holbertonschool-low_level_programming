89a9d52f68ccf9a76f017f354d4ddc7f4094eb3c
yuan-fang-228 <fy0228@gmail.com>
2022-05-26 20:47:55 -0700

    remove unnessary checking

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -32,11 +32,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	newnode = malloc(sizeof(*newnode));
 	if (newnode == NULL)
 		return (0);
-	if (strdup(key) == NULL)
-	{
-		free(newnode);
-		return (0);
-	}
 	newnode->key = strdup(key);
 	newnode->value = strdup(value);
 	newnode->next = ht->array[index];
