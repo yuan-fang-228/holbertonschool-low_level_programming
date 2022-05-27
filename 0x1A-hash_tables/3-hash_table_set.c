e179b4b2177b038fbfdb71244133137bf463845e
yuan-fang-228 <fy0228@gmail.com>
2022-05-26 20:43:42 -0700

    change logic a bit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -32,12 +32,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	newnode = malloc(sizeof(*newnode));
 	if (newnode == NULL)
 		return (0);
-	newnode->key = strdup(key);
-	if (newnode->key == NULL)
+	if (strdup(key) == NULL)
 	{
 		free(newnode);
 		return (0);
 	}
+	newnode->key = strdup(key);
 	newnode->value = strdup(value);
 	newnode->next = ht->array[index];
 	ht->array[index] = newnode;
