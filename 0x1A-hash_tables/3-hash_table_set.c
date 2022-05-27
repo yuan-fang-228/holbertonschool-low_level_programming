8d13c014d23d4022bbd539826ed813d078bc6ece
yuan-fang-228 <fy0228@gmail.com>
2022-05-26 20:56:22 -0700

    change if sequence

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *newnode = NULL;
 	unsigned long int index;
 
-	if (ht == NULL || key == NULL || strlen(key) == 0 || ht->array == NULL)
+	if (ht == NULL || ht->array == NULL || key == NULL || strlen(key) == 0)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
